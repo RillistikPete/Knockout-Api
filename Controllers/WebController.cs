2b99b4d7957ab53cff9a67a8048c493f4e4e685f
Gavin Forrest <Gavin.Forrest@NuOrigin.local>
2019-04-19 15:37:33 -0500

    first commit

---
+++ Controllers/WebController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class WebController : ApiController
+    {
+        private ReservationRepository repo = ReservationRepository.Current;
+
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll();
+        }
+
+        public Reservation GetReservation(int id)
+        {
+            return repo.Get(id);
+        }
+
+        [HttpPost]
+        public Reservation CreateReservation(Reservation reservation)
+        {
+            return repo.Add(reservation);
+        }
+
+        [HttpPut]
+        public bool UpdateReservation(Reservation reservation)
+        {
+            return repo.Update(reservation);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
---
+++ Models/ReservationRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServices.Models
+{
+    public class ReservationRepository
+    {
+        private static ReservationRepository repo = new ReservationRepository();
+
+        //To ensure there is some persistence b/w requests,
+        //create a static instance of this class, accessible through the Current property:
+        public static ReservationRepository Current
+        {
+            get
+            {
+                return repo;
+            }
+        }
+
+        private List<Reservation> data = new List<Reservation>
+        {
+            new Reservation {ReservationID=1, ClientName="Andrew", Location="Board Room"},
+            new Reservation {ReservationID=2, ClientName="Jackie", Location="Lecture Hall"},
+            new Reservation {ReservationID=3, ClientName="Russell", Location="Meeting Room"},
+        };
+
+        public IEnumerable<Reservation> GetAll()
+        {
+            return data;
+        }
+
+        public Reservation Get(int id)
+        {
+            return data.Where(r => r.ReservationID == id).FirstOrDefault();
+        }
+
+        public Reservation Add(Reservation reservation)
+        {
+            reservation.ReservationID = data.Count + 1;
+            data.Add(reservation);
+            return reservation;
+        }
+
+        public void Remove(int id)
+        {
+            Reservation reservation = Get(id);
+            if (reservation != null)
+            {
+                data.Remove(reservation);
+            }
+        }
+
+        public bool Update(Reservation reservation)
+        {
+            Reservation storedReservation = Get(reservation.ReservationID);
+            if(storedReservation != null)
+            {
+                storedReservation.ClientName = reservation.ClientName;
+                storedReservation.Location = reservation.Location;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
